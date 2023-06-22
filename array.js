77e99a8e7c7aed234ff8a01f99a6e5d44173ab3c
Ehinomwen <omoikeehinomwen@gmail.com>
2023-06-22 11:57:13 +0100

    A work on arrays

---
+++ array.js
@@ -0,0 +1,38 @@
+// let title = ["ID", "firstName", "LastName","Age", "salary", "depaertment"];
+// let staff1 = [1, "Ambrose", "Osagioduwa", 23, 40000, "IT" ];
+// let staff2 = [2, "Itohan", "Omosurowa", 30, 50000, "Media" ];
+
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write(title + "<br>")
+// document.write(staff1)
+// console.log(staff2)
+
+
+// document.write("<h3>Names of Employees</h3>")
+// document.write(title[1]+"<br>")
+// document.write(staff1[1]+"<br>")
+// document.write(staff1[2]+"<br>")
+
+
+// var counter = 0;
+// var lottery_numbers = new Array();
+
+// while (counter < 49) {
+//     lottery_numbers[counter] = counter +1;
+//     document.write(lottery_numbers[counter] + "<br>");
+//     counter++;
+// }
+
+
+var counter = 0;
+var multiplier = new Array();
+document.write("<h1 >MULTIPLICATION BY 2<h1/>")
+while (counter < 52) {
+    multiplier[counter] = counter + 1;
+    document.write("2 x" + multiplier[counter] + " = " +(multiplier[counter]) *2 + "<br>");
+    counter++;
+    
+    
+} 
